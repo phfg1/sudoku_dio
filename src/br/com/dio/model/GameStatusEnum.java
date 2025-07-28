26bc8ad24b93786e5d4c1e5694075dc3f70b9ba2
pedro <pdohfg@gmail.com>
2025-07-28 19:05:03 -0300

    criando repositório e enviando arquivos de jogo completo e funcional

---
+++ src/br/com/dio/model/GameStatusEnum.java
@@ -0,0 +1,17 @@
+package br.com.dio.model;
+
+public enum GameStatusEnum {
+    NON_STARTED("Não iniciado"),
+    INCOMPLETE("Incompleto"),
+    COMPLETE("Completo");
+
+   private String label;
+
+   GameStatusEnum(final String label) {
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+}
